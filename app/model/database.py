2b87f4d56f1db02c1551c205a330cd51d36c5fbd
djamelghorab <djamel.ghorab@gmail.com>
2026-04-26 22:55:42 +0100

    Edited  database connection

--- app/model/database.py
+++ app/model/database.py
@@ -1,21 +1,28 @@
 import os
 from dotenv import load_dotenv
-from pymongo.mongo_client import MongoClient
+from pymongo import MongoClient, ASCENDING
 from pymongo.server_api import ServerApi
 
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
-DATABASE_NAME = os.getenv("DATABASE_NAME")
-COLLECTION_NAME = os.getenv("COLLECTION_NAME")
-
-
-def get_database():
-    client = MongoClient(MONGO_URI, server_api=ServerApi("1"))
-    client.admin.command("ping")
-    return client[DATABASE_NAME]
+DATABASE_NAME = os.getenv("DATABASE_NAME", "user_management_db")
+COLLECTION_NAME = os.getenv("COLLECTION_NAME", "users")
 
 
 def get_users_collection():
-    db = get_database()
-    return db[COLLECTION_NAME]
+    if not MONGO_URI:
+        raise ValueError("MONGO_URI is missing from .env file")
+
+    client = MongoClient(MONGO_URI, server_api=ServerApi("1"))
+    client.admin.command("ping")
+
+    db = client[DATABASE_NAME]
+    users_collection = db[COLLECTION_NAME]
+
+    users_collection.create_index(
+        [("phone_number", ASCENDING)],
+        unique=True
+    )
+
+    return users_collection
